7024aafca42f9468dcdce20a8aa3f180ddeca468
johnpooler <johnpooler98@gmail.com>
2020-03-11 12:46:58 +0000

    crud with 1.Main/ 2.Helper/ 3.case class Person/ 4. services

---
+++ src/main/scala/PersonMain.scala
@@ -0,0 +1,74 @@
+import com.qa.helpers.MongoHelper
+import com.qa.models.Person
+import org.mongodb.scala.{MongoClient, MongoCollection, MongoDatabase}
+import org.mongodb.scala.bson.codecs.Macros._
+import org.mongodb.scala.bson.codecs.DEFAULT_CODEC_REGISTRY
+import org.bson.codecs.configuration.CodecRegistries.{fromProviders, fromRegistries}
+import org.mongodb.scala.bson.ObjectId
+import org.mongodb.scala.model.Filters.equal
+import org.mongodb.scala.model.Updates.set
+import scala.util.{Failure, Success}
+import scala.concurrent.ExecutionContext.Implicits.global
+
+object PersonMain extends App {
+
+  val codecRegistry = fromRegistries(fromProviders(classOf[Person]), DEFAULT_CODEC_REGISTRY )
+  val mongoClient: MongoClient = MongoClient()
+  val database: MongoDatabase = mongoClient.getDatabase("people").withCodecRegistry(codecRegistry)
+  val collection: MongoCollection[Person] = database.getCollection("people")
+
+  def addDocument(doc: Person): Unit = {
+    collection.insertOne(doc)
+      .subscribe(MongoHelper.observeInsert)
+  }
+
+  def addDocuments(doc: IndexedSeq[Person]): Unit = {
+    collection.insertMany(doc)
+      .subscribe(MongoHelper.observeInsert)
+  }
+
+  def getAllDocuments = {
+    collection.find.toFuture
+  }
+
+  def getFirstDocument = {
+    collection.find.first.headOption
+  }
+
+  def deleteById(id: ObjectId): Unit = {
+    collection.deleteOne(equal("_id", id)).toFuture.onComplete {
+      case Success(_) => println("Completed")
+      case Failure(error) => println(error)
+    }
+  }
+
+
+  def findById(id: ObjectId): Unit = {
+    findByIdReturningFuture(id).onComplete {
+      case Success(value) => println(s"The value we've been waiting for is: ${value.getOrElse("Item not found")}")
+      case Failure(error) => println(error)
+    }
+  }
+
+  def findByIdReturningFuture(id: ObjectId) = {
+    collection.find(equal("_id", id)).headOption
+  }
+
+  def updateName(id: ObjectId, newName: String): Unit = {
+    collection.updateOne(equal("_id", id), set("name", newName)).toFuture.onComplete {
+      case Success(value) => println(s"The value has been updated to: $value")
+      case Failure(error) => println(error)
+    }
+  }
+
+  addDocument(Person("Jeronimo", "Stilton"))
+  //getFirstDocument.map(value => println(value.getOrElse("Value not found")))
+  //deleteById(new ObjectId("5e68d9fcb22d361aa401322a"))
+  getAllDocuments.map(println(_))
+
+
+
+  Thread.sleep(5000)
+  mongoClient.close()
+
+}
---
+++ src/main/scala/com/qa/models/Person.scala
@@ -0,0 +1,14 @@
+package com.qa.models
+
+import org.mongodb.scala.bson.ObjectId
+
+object Person {
+
+  def apply(firstName: String, lastName: String): Person = Person(new ObjectId(), firstName, lastName)
+}
+
+
+
+case class Person(_id: ObjectId, firstName: String, lastName: String)
+
+
